e093e473a719e70103f2eb712ef318dd608cbcbc
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-23 12:44:59 +0300

    complete

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,15 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number= wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
+# v1 = Vehicle("large", 4)
+# print(v1.wheel_size)  
+# print(v1.wheel_number)        
+
